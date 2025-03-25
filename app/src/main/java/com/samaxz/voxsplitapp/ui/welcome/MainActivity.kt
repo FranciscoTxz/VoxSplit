ba42db58b0ea4964c1ef8e9bb04c257100a42c52
FranciscoTxz <angelftv2002@hotmail.com>
2025-03-25 15:32:11 -0600

    New Menu Activity

--- app/src/main/java/com/samaxz/voxsplitapp/ui/welcome/MainActivity.kt
+++ app/src/main/java/com/samaxz/voxsplitapp/ui/welcome/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.samaxz.voxsplitapp.ui.welcome
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.samaxz.voxsplitapp.databinding.ActivityMainBinding
+import com.samaxz.voxsplitapp.ui.menu.MenuActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,5 +15,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        initListeners()
+    }
+
+    private fun initListeners() {
+        val btnNavigate: Button = binding.btnWelcome
+        btnNavigate.setOnClickListener {
+            // Navigate next menu activity
+            val intent = Intent(this, MenuActivity::class.java)
+            //intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+
+        }
     }
 }
